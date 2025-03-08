573f5f87f3197db915853ed796f2415a66d17a97
getError <getError@163.com>
2025-03-08 21:59:37 +0800

    可以用了

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import asyncio
+import math
+
 import websockets
 import pyautogui
+import win32api
+import win32con
 import json
 from websockets.server import serve
 import threading
@@ -12,24 +16,34 @@ threadPool = ThreadPoolExecutor(max_workers=20, thread_name_prefix="test_")
 
 pyautogui.PAUSE = 0.005
 
+MAX_MOVE_SIZE = 50
+
+
+def move_mouse_relative(x_offset, y_offset):
+    # 获取当前鼠标的位置
+    current_x, current_y = win32api.GetCursorPos()
+    # 计算新的鼠标位置
+    new_x = current_x + x_offset
+    new_y = current_y + y_offset
+    # 设置鼠标到新的位置
+    win32api.SetCursorPos((new_x, new_y))
+
 
 async def handle_client(websocket):
     lock = threading.Lock()
 
     def process_move(data):
-        if lock.acquire(blocking=False):
-            try:
-                screen = get_monitors()[0]
-                x = data['x'] * screen.width * 0.1
-                y = data['y'] * screen.height * 0.1
-                # 计时
-                pyautogui.moveRel(x, y)
-            except Exception as e:
-                print(f"Error: {e}")
-            finally:
-                lock.release()
-                return
-        return
+        try:
+            screen = get_monitors()[0]
+            x = data['x'] * screen.width * 0.9
+            y = data['y'] * screen.height * 0.9
+            # 计时
+            # pyautogui.moveRel(x, y)
+            move_mouse_relative(min(int(x), int(math.copysign(x, MAX_MOVE_SIZE))), min(int(y), int(math.copysign(y, MAX_MOVE_SIZE))))
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            return
 
     async def process_click(data):
         button = 'left' if data['button'] == 0 else 'right'
@@ -41,7 +55,8 @@ async def handle_client(websocket):
         try:
             data = json.loads(message)
             if data['type'] == 'move':
-                threadPool.submit(process_move, data)
+                # threadPool.submit(process_move, data)
+                process_move(data)
             elif data['type'] == 'click':
                 await process_click(data)
         except Exception as e:
@@ -58,3 +73,7 @@ async def main():
 if __name__ == "__main__":
     print("Starting server...")
     asyncio.run(main())
+
+if __name__ == "__main__1":
+    for i in range(1, 192):
+        win32api.SetCursorPos((i, 500))
